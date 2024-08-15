31d208408dc16e206904acb1f22488846d50c060
AbhishekPandey10 <10072001ap@gmail.com>
2024-08-15 22:03:29 +0530

    -

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -5,12 +5,14 @@ import { postReducer } from "./Post/post.reducer";
 import commentReducer from "./Comment/comment.reducer";
 import { reelsReducer } from "./Reels/reels.reducer";
 import { messagesReducer } from "./Message/message.reducers";
+import jobsReducer from "../pages/Jobs/jobs.reducer";
 
 const rootReducers = combineReducers({
   auth: authReducer,
   post: postReducer,
   comment:commentReducer,
   reel:reelsReducer,
-  chat:messagesReducer
+  chat:messagesReducer,
+  jobs: jobsReducer,
 });
 export const store = legacy_createStore(rootReducers, applyMiddleware(thunk));
