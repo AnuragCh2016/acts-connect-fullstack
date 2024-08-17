d71b830e7fbbd9e8dea09238ca9b3215aa85fe0b
anurag <anuragch198@gmail.com>
2024-08-17 18:18:18 +0530

    corrected backend api url

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 const jwtToken = localStorage.getItem("jwt")
-export const API_BASE_URL = 'http://localhost:5454';
+export const API_BASE_URL = 'http://localhost:8080';
 
 
 export const api = axios.create({
-  baseURL: API_BASE_URL, 
+  baseURL: API_BASE_URL,
   headers: {
-    'Authorization':`Bearer ${jwtToken}`,
+    'Authorization': `Bearer ${jwtToken}`,
     'Content-Type': 'application/json',
   },
 });
